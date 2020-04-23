2b7ce1a4a3abc86e9c02baa201f13a46d5511751
Max Arshinov <max@hightech.group>
2020-04-23 18:37:39 +0300

    Introduce MathOperation

--- CalcRefactoring/CalcRefactoring/Program.cs
+++ CalcRefactoring/CalcRefactoring/Program.cs
@@ -15,35 +15,36 @@ namespace CalcRefactoring
         #warning Нет тестов
         static void Main(string[] args)
         {
-            #warning Exception
-            var val1 = GetDouble("Please enter val1");
-
-            #warning Дублирование
-            Console.WriteLine("Please enter operation:");
-            var operation = Console.ReadLine();
-
-            #warning Exception
-            var val2 = GetDouble("Please enter val2");
+            var val1 = GetValue<double>("Please enter val1");
+            var operation = GetValue<MathOperation>("Please enter operation");
+            var val2 = GetValue<double>("Please enter val2");
 
             double result = 0;
             
-            #warning Не нравится (вкусовщина)
             #warning Exception
+            #warning Не нравится (вкусовщина)
             switch (operation)
             {
                 #warning Плохая читаемость
-                case "+": result = val1 + val2; break; 
-                case "-": result = val1 - val2; break;
+                case MathOperation.Plus: result = val1 + val2; break; 
+                case MathOperation.Minus: result = val1 - val2; break;
             }
             
             Console.WriteLine($"Result is: {result}");
         }
 
-        private static double GetDouble(string message)
+        private static T GetValue<T>(string message)
         {
             Console.WriteLine(message + ":");
-            var val1 = Double.Parse(Console.ReadLine());
-            return val1;
+            #warning Exception
+            #warning Performance (boxing/unboxing) 
+            return (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
         }
+    }
+
+    public enum MathOperation
+    {
+        Plus,
+        Minus
     }
 }
