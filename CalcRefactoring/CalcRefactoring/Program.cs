1bbe0305ffd41bbdb782d9c5447e4d097ed8eee2
Max Arshinov <max@hightech.group>
2020-04-23 18:08:52 +0300

    analysis

--- CalcRefactoring/CalcRefactoring/Program.cs
+++ CalcRefactoring/CalcRefactoring/Program.cs
@@ -4,20 +4,32 @@ namespace CalcRefactoring
 {
     class Program
     {
+        // + Заработал с первого раза
+        // + Мне было очень
+        // + Сделал очень быстро
+        #warning Нет тестов
         static void Main(string[] args)
         {
+            #warning Дублирование
             Console.WriteLine("Please enter val1:");
+            #warning Exception
             var val1 = Double.Parse(Console.ReadLine());
             
+            #warning Дублирование
             Console.WriteLine("Please enter operation:");
             var operation = Console.ReadLine();
 
+            #warning Дублирование
             Console.WriteLine("Please enter val2:");
+            #warning Exception
             var val2 = Double.Parse(Console.ReadLine());
 
             double result = 0;
+            #warning Не нравится (вкусовщина)
+            #warning Exception
             switch (operation)
             {
+                #warning Плохая читаемость
                 case "+": result = val1 + val2; break; 
                 case "-": result = val1 - val2; break;
             }
